f9fad7c141fe5b000557fd2f953d092e88495dc4
Nick-Sugar <nicksugar0141@gmail.com>
2020-12-22 12:36:09 +0900

    修正 menber.py change　test2 -> datas

--- memberlist.py
+++ memberlist.py
@@ -6,10 +6,10 @@ import lxml.html
 
 def start():
 	r = urllib.request.urlopen("https://nijisanji.ichikara.co.jp/member/")
-	test2 = lxml.html.fromstring(r.read())
+	datas = lxml.html.fromstring(r.read())
 
-	for i in range(1, len(test2.xpath(f'//*[@id="liver_list"]/div')) + 1):
-		datas = test2.xpath(f'//*[@id="liver_list"]/div[{i}]/div/div/a/span')
+	for i in range(1, len(datas.xpath(f'//*[@id="liver_list"]/div')) + 1):
+		datas = datas.xpath(f'//*[@id="liver_list"]/div[{i}]/div/div/a/span')
 		print(datas[0].text.replace("\n",""))
 
 start()
